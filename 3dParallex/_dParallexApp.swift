6441118cc2894daef07e171951ee80a9a4f61b5f
Ripon Seikh <34369083+riponsk512@users.noreply.github.com>
2024-02-24 21:05:49 +0530

    Initial Commit

---
+++ 3dParallex/_dParallexApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dParallexApp.swift
+//  3dParallex
+//
+//  Created by Ripon sk on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct _dParallexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
